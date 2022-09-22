22a9b3b8fbe2f3c4790d2ccaf76cc6dbd31d1d67
Pablo Velasquez <pevb97@gmail.com>
2022-09-22 09:21:54 -0500

    Resubiendo MVC Permisos

--- Seguridad/src/main/java/seguridadRegistraduriaGP2/Seguridad/Repositorios/RepositorioPermiso.java
+++ Seguridad/src/main/java/seguridadRegistraduriaGP2/Seguridad/Repositorios/RepositorioPermiso.java
@@ -1,2 +1,11 @@
-package seguridadRegistraduriaGP2.Seguridad.Repositorios;public interface RepositorioPermiso {
+package seguridadRegistraduriaGP2.Seguridad.Repositorios;
+
+
+import org.springframework.data.mongodb.repository.Query;
+import seguridadRegistraduriaGP2.Seguridad.Modelos.Permiso;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RepositorioPermiso extends MongoRepository<Permiso, String> {
+    @Query("{'url':?0,'metodo':?1}")
+    Permiso getPermiso(String url, String metodo);
 }
